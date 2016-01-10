436875ad27dc60766da978c22837d7be7d5251b2
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2016-01-10 18:28:52 -0500

    first commit

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero1=5;
+$numero2=15;
+$resultado=$numero1+$numero2;
+
+echo $numero1." + ".$numero2."=";
+echo $resultado;
+ 
+ ?>
---
+++ php/miperfil.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $nombre="jorge";
+
+ $apelido="meza";
+
+ $fecha=1982;
+
+ $edad=2016-$fecha;
+ echo "nombre :".$nombre;
+ echo "</br>";
+ echo "apellidos :".$apelido;
+ echo "</br>";
+echo "fecha de nacimiento:".$fecha;
+ echo "</br>";
+ echo "mi edad es :".$edad;
+ ?>
